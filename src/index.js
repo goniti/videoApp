7acf2df48cca155e5ec05955977f55e729edb091
Carlos <goniti@live.fr>
2018-10-30 22:31:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/app'
 
-const API_KEY ="dfe1a47ecdd5ff958cffd8599d8da6b3";
+const API_KEY ="..YOUR...KEY.....";
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
 
